f265a2c5a43338a9e1192d676b2cdcb79fd77358
RonaldoFM <43863628+RonaldoFM@users.noreply.github.com>
2025-01-09 22:37:24 -0300

    Sessao 15

--- src/Services/Ordering/Ordering.Domain/Models/Order.cs
+++ src/Services/Ordering/Ordering.Domain/Models/Order.cs
@@ -7,7 +7,7 @@
 
         public CustomerId CustomerId { get; private set; } = default!;
         public OrderName OrderName { get; private set; } = default!;
-        public Address ShipingAddress { get; private set; } = default!;
+        public Address ShippingAddress { get; private set; } = default!;
         public Address BillingAddress { get; private set; } = default!;
         public Payment Payment { get; private set; } = default!;
         public OrderStatus Status { get; private set; } = OrderStatus.Pending;
@@ -17,14 +17,14 @@
             private set { }
         }
 
-        public static Order Create(OrderId id, CustomerId customerId, OrderName orderName, Address shipingAddress, Address billingAddress, Payment payment)
+        public static Order Create(OrderId id, CustomerId customerId, OrderName orderName, Address shippingAddress, Address billingAddress, Payment payment)
         {
             var order = new Order
             {
                 Id = id,
                 CustomerId = customerId,
                 OrderName = orderName,
-                ShipingAddress = shipingAddress,
+                ShippingAddress = shippingAddress,
                 BillingAddress = billingAddress,
                 Payment = payment,
                 Status = OrderStatus.Pending
@@ -35,10 +35,10 @@
             return order;
         }
 
-        public void Update(OrderName orderName, Address shipingAddress, Address billingAddress, Payment payment, OrderStatus status)
+        public void Update(OrderName orderName, Address shippingAddress, Address billingAddress, Payment payment, OrderStatus status)
         {
             OrderName = orderName;
-            ShipingAddress = shipingAddress;
+            ShippingAddress = shippingAddress;
             BillingAddress = billingAddress;
             Payment = payment;
             Status = status;
